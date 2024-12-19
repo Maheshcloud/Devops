21297810ee294014f25200314bf1a91332706e13
Maheshcloud <mahesh.cloud84@gmail.com>
2024-12-19 14:11:28 +0530

    deleted unwanted files

--- scripts/terraform/terraform-docker/output.tf
+++
@@ -1,3 +0,0 @@
-output "jenkins_endpoint" {
-  value = formatlist("http://%s:%s/", aws_instance.DockerEC2.*.public_ip, "8080")
-}
--- scripts/terraform/terraform-jenkins/output.tf
+++
@@ -1,3 +0,0 @@
-output "jenkins_endpoint" {
-  value = formatlist("http://%s:%s/", aws_instance.JenkinsEC2.*.public_ip, "8080")
-}
